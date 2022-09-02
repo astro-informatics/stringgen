5b2abcfcd08ab082affd3ed10445d11c2a15dfda
Matthijs Mars <mm6@msslxai.mssl.ucl.ac.uk>
2022-09-02 12:54:17 +0100

    adds basic test to see if code runs

---
+++ cosmic_string_emulator/tests/test_CosmicStringEmulator.py
@@ -0,0 +1,22 @@
+
+import numpy as np
+
+from cosmic_string_emulator import CosmicStringEmulator
+
+def test_cse():
+    """Tests a very simple/small emulation case to see whether all functions run through without any issues. 
+    """
+    emulator = CosmicStringEmulator(
+        emulation_shape=(32, 32),
+        J=4,
+        L=4,
+        dn=1,
+        norm="auto",
+        pbc=True,
+        cplx=False,
+        device=0,
+    )
+
+    im = np.random.rand(1,32,32)
+    features = emulator.generate_features(im)
+    emulation = emulator.emulate(features, n_emulations=1, max_iterations=3)
