89289c59796d7db0f8fcc3a8bde3143ae69ed490
Matthijs Mars <mm6@msslxai.mssl.ucl.ac.uk>
2022-09-02 12:52:01 +0100

    adds setup files

---
+++ setup.py
@@ -0,0 +1,52 @@
+import os
+import shutil
+from setuptools import setup
+
+import numpy
+
+# clean previous build
+for root, dirs, files in os.walk("./cosmic_string_emulator/", topdown=False):
+    for name in dirs:
+        if (name == "build"):
+            shutil.rmtree(name)
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+
+def read_requirements(file):
+    with open(file) as f:
+        return f.read().splitlines()
+
+def read_file(file):
+   with open(file) as f:
+        return f.read()
+
+long_description = read_file("README.md")
+required = read_requirements("requirements.txt")
+
+
+include_dirs = [numpy.get_include(),]
+
+extra_link_args=[]
+
+setup(
+    classifiers=['Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Operating System :: OS Independent',
+                 'Intended Audience :: Developers',
+                 'Intended Audience :: Science/Research'
+                 ],
+    name = "cosmic_string_emulator",
+    version = "0.0.1",
+    prefix='.',
+    url='https://github.com/astro-informatics/cosmic_string_emulator',
+    author='Matthew Price, Matthijs Mars, Matthew Docherty, Auggie Marignier, Jason McEwen & Contributers',
+    # author_email='',
+    # license='GNU General Public License v3 (GPLv3)',
+    install_requires=required,
+    description='Cosmic String Emulator',
+    long_description_content_type = "text/markdown",
+    long_description=long_description,
+    packages=['cosmic_string_emulator']
+)
